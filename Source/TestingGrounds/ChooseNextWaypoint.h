02b987fba33664c2257f257f399adf634e66740d
Shua11 <math.joshua11@gmail.com>
2018-11-30 10:29:33 -0700

    Moving Patrol to C++. Removed Patrol Guard Class.

--- Source/TestingGrounds/ChooseNextWaypoint.h
+++ Source/TestingGrounds/ChooseNextWaypoint.h
@@ -18,8 +18,8 @@ class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 
 protected:
 	UPROPERTY(EditAnywhere, Category = "Blackboard")
-		struct FBlackboardKeySelector IndexKey;
+	struct FBlackboardKeySelector IndexKey;
 	
 	UPROPERTY(EditAnywhere, Category = "Blackboard")
-		struct FBlackboardKeySelector Waypoint;
+	struct FBlackboardKeySelector WaypointKey;
 };
